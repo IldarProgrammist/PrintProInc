133b8fd8b4d15d74629be3db02cde0bbc94a1eb8
ildar <ildar@DESKTOP-VRJDR41>
2020-03-30 11:59:54 +0300

    Add function  load Printer operation

--- Clasess/WorkInOperationPrinter.cs
+++ Clasess/WorkInOperationPrinter.cs
@@ -1,22 +1,44 @@
-﻿using PrintProInc.Work;
+﻿using MetroFramework.Controls;
+using PrintProInc.Models;
+using PrintProInc.Work;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PrintProInc.Clasess
 {
     class WorkInOperationPrinter : IOperations
     {
+        private MetroGrid Dgv { get; set; }
+
         public void count()
         {
             throw new NotImplementedException();
         }
 
+        public WorkInOperationPrinter(MetroGrid dgv)
+        {
+            Dgv = dgv;
+        }
+
+
         public void load()
         {
+            using(ContextModel db = new ContextModel())
+            {
+
+                var OperatonInfo = from op in db.PrinterOperation
+                                    select new
+                                    {
+                                        op.PrinterID,
+                                        op.Printer.SerialNamber,
+                                        op.PrinterStatus.PrinterName,
+                                        
+                                    };
+                Dgv.DataSource = OperatonInfo.ToList();
+
+            }
             
+
         }
 
         public void search()
